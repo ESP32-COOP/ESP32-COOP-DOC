91b6da28d0245ae213f557f524ee3dbe420cb0b3
qypol342 <qypol342@gmail.com>
2023-02-11 14:38:47 +0100

    feat(home): overall improved

--- COOP-DOOR-WEB/src/routes/+page.svelte
+++ COOP-DOOR-WEB/src/routes/+page.svelte
@@ -13,17 +13,18 @@
     }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-2 h-screen grid grid-cols-1 " style="grid-template-rows : 1fr 1fr 6fr 3fr;">
+    <p>button</p>
+    <h1 class="uppercase text-3xl ml-8">coop <span class="font-bold">door</span></h1>
+    <div class="">
 
-<a href="settings"><BigTitle/></a>
+    </div>
+    <div class="h-full mx-4    grid grid-cols-2 gap-4 w-auto h-40">
 
-<button on:click={() => {count++;}}>add 1</button>
-
-<p> test count = {count}</p>
-<div class="mx-4    grid grid-cols-2 gap-4 w-auto h-40">
-
-<SmallBadge { ...SmallBadgeData} value={count} />
-<SmallBadge { ...SmallBadgeData} value={count} />
+        <SmallBadge { ...SmallBadgeData} value={count} />
+        <SmallBadge { ...SmallBadgeData} value={count} />
+        
+    </div>
 
 </div>
+
