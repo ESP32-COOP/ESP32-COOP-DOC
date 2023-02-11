c53db35eb3e97ed2b91abf7a4280cf676de116ea
qypol342 <qypol342@gmail.com>
2023-02-11 19:40:37 +0100

    feat(home): add icon and better button

--- COOP-DOOR-WEB/src/routes/+layout.svelte
+++ COOP-DOOR-WEB/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 
-<div class="bg-cover absolute top-0 w-screen h-screen overflow-hidden bg-[url('/back3.svg')] -z-10">
+<div class="bg-cover absolute top-0 w-screen h-screen overflow-hidden bg-[url('/back3.svg')] -z-10 bg-slate-200">
 
 </div> 
 
--- COOP-DOOR-WEB/src/routes/+page.svelte
+++ COOP-DOOR-WEB/src/routes/+page.svelte
@@ -4,11 +4,20 @@
 
     let count = 1;
 
-    const SmallBadgeData = {
+    const lightSmallBadgeData = {
         title: 'Light Sensor',
         subTitle: 'britness level',
         subTitleType: "value",
         value: count,
+        badgeType: "dark"
+
+    }
+
+    const dateSmallBadgeData = {
+        title: 'Date Time',
+        subTitle: 'britness level',
+        subTitleType: "value",
+        icon: "clock",
 
     }
 </script>
@@ -16,18 +25,18 @@
 <div class="p-2 h-screen grid grid-cols-1 " style="grid-template-rows : 1fr 1fr 8fr 3fr;">
     <div class="w-full h-full flex flex-row items-center">
         <div class=" ml-auto bg-opacity-80 bg-slate-300 h-fit m-2 p-2 rounded-full">
-            <img src="gear.svg">
+            <img src="gear.svg" alt="gear">
         </div>
     </div>
 
     <h1 class="uppercase text-3xl ml-8">coop <span class="font-bold">door</span></h1>
-    <div class="">
+    <div class="bg-[url('/coop.svg')] bg-no-repeat bg-center">
 
     </div>
-    <div class="h-full mx-4    grid grid-cols-2 gap-4 w-auto h-40">
+    <div class="h-full mx-4    grid grid-cols-2 gap-4 w-auto">
 
-        <SmallBadge { ...SmallBadgeData} value={count} />
-        <SmallBadge { ...SmallBadgeData} value={count} />
+        <SmallBadge { ...lightSmallBadgeData} value={count} />
+        <SmallBadge { ...dateSmallBadgeData} value={count} />
         
     </div>
 
