25c9a5047c2af029574c29ab253a0cab01c6abcf
qypol342 <qypol342@gmail.com>
2023-02-15 14:06:23 +0100

    feat(settings): start layout

--- COOP-DOOR-WEB/src/routes/settings/+page.svelte
+++ COOP-DOOR-WEB/src/routes/settings/+page.svelte
@@ -8,7 +8,13 @@
     </div>
 <h1 class="uppercase text-3xl ml-8">coop <span class="font-bold">settings</span></h1>
 <div class="w-full h-full flex items-center justify-center">
-    <div class="rounded-2xl bg-slate-300 opacity-70 w-4/5 h-4/5">
+    <div class="rounded-2xl bg-slate-300 opacity-70 w-4/5 h-4/5 grid grid-cols-1 gap-5 p-5" style="grid-template-rows : 2fr 2fr 5fr 1fr 5fr 2fr ;">
+        <div class="bg-slate-50 rounded-xl"></div>
+        <div class="bg-slate-50 rounded-xl"></div>
+        <div class="bg-slate-50 rounded-xl"></div>
+        <div class="bg-slate-50 rounded-xl"></div>
+        <div class="bg-slate-50 rounded-xl"></div>
+        <div class="bg-slate-50 rounded-xl"></div>
 
     </div>
 </div>
