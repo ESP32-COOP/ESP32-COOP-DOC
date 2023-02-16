9f90aacef696f12d57afb1f2c74bb3cfb0cb1a1e
qypol342 <qypol342@gmail.com>
2023-02-16 17:52:54 +0100

    fix(light-page): fix mobile layout

--- COOP-DOOR-WEB/src/routes/cards/light/+page.svelte
+++ COOP-DOOR-WEB/src/routes/cards/light/+page.svelte
@@ -25,20 +25,21 @@ onMount( async ()=>{
 <h1 class="uppercase text-3xl ml-8">light <span class="font-bold">sensor</span></h1>
 <div class="w-full h-full flex items-center justify-center">
     <div class="rounded-2xl bg-gradient-to-r  from-blue-900 to-slate-900 opacity-70 w-4/5 h-[60%]">
-        <div class="grid w-full h-full" style="grid-template-rows : 2fr 8fr 2fr ">
+        <div class="grid w-full h-full gap-5 p-5" style="grid-template-rows : 2fr 8fr 2fr ">
             <div class="w-full h-full flex items-center justify-center">
-                <div class="rounded-2xl bg-slate-200/20  m-5 w-full h-auto flex items-center ">
-                    <div style="width: {lightValue}%;" class=" rounded-xl m-2 bg-gradient-to-r from-red-400 to-yellow-200  h-20  transition-all">
+                <div class="rounded-2xl bg-slate-200/20   w-full h-full
+                 flex items-center ">
+                    <div style="width: {lightValue}%;" class=" rounded-xl  bg-gradient-to-r from-red-400 to-yellow-200  h-full  transition-all">
                     </div>
                 </div>
             </div>
-            <div class="grid w-full h-full columns-1">
+            <div class="grid w-full h-full columns-1 gap-5">
                 <div class="w-full h-full flex items-center justify-center">
-                    <div class="rounded-2xl bg-slate-200/20 m-5 w-full h-full grid grid-cols-2 items-center justify-center gap-3">
+                    <div class="rounded-2xl bg-slate-200/20  w-full h-full grid grid-cols-2 items-center justify-center gap-3">
                         <p class="text-white/60 text-xl text-right">value : </p><h2 class="text-white text-5xl">{lightValue}</h2>
                     </div>
                 </div>
-                <div class="w-auto h-full columns-2 m-5 gap-5">
+                <div class="w-auto h-full columns-2  gap-5">
                     <div class="rounded-2xl bg-slate-200/20  w-full h-full flex flex-col justify-center items-center">
                         <h3 class="text-white text-4xl">{minValue}</h3>
                         <p class="text-white/60">min</p>
@@ -54,7 +55,7 @@ onMount( async ()=>{
 
             </div>
             <div class="flex items-center justify-center w-full h-full">
-                <button class="uppercase bg-slate-500/50 rounded-xl m-5 w-full h-24 text-white text-2xl " >reset</button>
+                <button class="uppercase bg-slate-500/50 rounded-xl  w-full h-full text-white text-2xl " >reset</button>
             </div>
         </div>
 
